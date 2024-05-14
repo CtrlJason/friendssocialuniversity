66f6c1c590c4ae0e52a0217b581bc5c131702ba3
juancamilop1 <159964422+juancamilop1@users.noreply.github.com>
2024-05-14 15:01:30 -0500

    arreglo de base de datos

--- bases de datos/id22016913_socialuniversityfriends.sql
+++ bases de datos/id22016913_socialuniversityfriends.sql
@@ -24,203 +24,84 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `carrera`
---
-
-CREATE TABLE `carrera` (
-  `idcarrera` int(11) NOT NULL,
-  `carrera` varchar(100) DEFAULT NULL,
-  `semestre` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `chat`
---
-
-CREATE TABLE `chat` (
-  `id` int(11) NOT NULL,
-  `enviado` int(11) DEFAULT NULL,
-  `recibido` int(11) DEFAULT NULL,
-  `mensaje` text DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- --------------------------------------------------------
-
---
 -- Estructura de tabla para la tabla `genero`
 --
 
-CREATE TABLE `genero` (
-  `idgenero` int(11) NOT NULL,
-  `genero` varchar(100) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+CREATE TABLE genero (
+    idgenero INT AUTO_INCREMENT PRIMARY KEY,
+    genero VARCHAR(100)
+);
 
--- --------------------------------------------------------
+--
+-- Estructura de tabla para la tabla `carrera`
+--
+CREATE TABLE carrera (
+    idcarrera INT AUTO_INCREMENT PRIMARY KEY,
+    carrera VARCHAR(100),
+    semestre INT
+);
+
+--
+-- Estructura de tabla para la tabla `registro`
+--
+CREATE TABLE registro (
+    codigoestudiante INT PRIMARY KEY NOT NULL,
+    nombre VARCHAR(100),
+    apellido VARCHAR(100), 
+    correo VARCHAR(100),
+    contraseña VARCHAR(100),
+    nick VARCHAR(100) UNIQUE NOT NULL,
+    idcarrera INT,
+    fechadenacimiento DATE,
+    idgenero INT,
+    FOREIGN KEY (idcarrera) REFERENCES carrera(idcarrera),
+    FOREIGN KEY (idgenero) REFERENCES genero(idgenero) 
+);
 
 --
 -- Estructura de tabla para la tabla `gustos`
 --
 
-CREATE TABLE `gustos` (
-  `idgustos` int(11) NOT NULL,
-  `gustos` varchar(100) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `registro`
---
-
-CREATE TABLE `registro` (
-  `codigoestudiante` int(11) NOT NULL,
-  `nombre` varchar(100) DEFAULT NULL,
-  `apellido` varchar(100) DEFAULT NULL,
-  `correo` varchar(100) DEFAULT NULL,
-  `contraseña` varchar(100) DEFAULT NULL,
-  `nick` varchar(100) NOT NULL,
-  `idcarrera` int(11) DEFAULT NULL,
-  `fechadenacimiento` date DEFAULT NULL,
-  `idgenero` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- --------------------------------------------------------
-
+CREATE TABLE gustos (
+    idgustos INT AUTO_INCREMENT PRIMARY KEY,
+    gustos VARCHAR(100)
+);
 --
 -- Estructura de tabla para la tabla `usuario`
 --
+CREATE TABLE usuario (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    idestudiante INT,
+    idgustos INT,
+    idgenerousuario INT,
+    idcarrerausuario INT,
+    FOREIGN KEY (idestudiante) REFERENCES registro(codigoestudiante),
+    FOREIGN KEY (idgustos) REFERENCES gustos(idgustos),
+    FOREIGN KEY (idgenerousuario) REFERENCES genero(idgenero),
+    FOREIGN KEY (idcarrerausuario) REFERENCES carrera(idcarrera)
+);
 
-CREATE TABLE `usuario` (
-  `id` int(11) NOT NULL,
-  `idestudiante` int(11) DEFAULT NULL,
-  `idgustos` int(11) DEFAULT NULL,
-  `idgenerousuario` int(11) DEFAULT NULL,
-  `idcarrerausuario` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- --------------------------------------------------------
-
+--
+-- Estructura de tabla para la tabla `chat`
+--
+CREATE TABLE chat (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    enviado INT,
+    recibido INT,
+    mensaje TEXT,
+    FOREIGN KEY (enviado) REFERENCES registro(codigoestudiante),
+    FOREIGN KEY (recibido) REFERENCES registro(codigoestudiante)
+);
 --
 -- Estructura de tabla para la tabla `notificaciones`
 --
- 
-CREATE TABLE `notificaciones` (
-  `idnotificacion` int(11) PRIMARY KEY,
-  `idusuario` int(25),
-  `tipodenotificacion` varchar(50),
-  FOREIGN KEY (`idusuario`) REFERENCES `usuario` (`id`)
-);
---
--- Índices para tablas volcadas
---
 
---
--- Indices de la tabla `carrera`
---
-ALTER TABLE `carrera`
-  ADD PRIMARY KEY (`idcarrera`);
-
---
--- Indices de la tabla `chat`
---
-ALTER TABLE `chat`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `enviado` (`enviado`),
-  ADD KEY `recibido` (`recibido`);
-
---
--- Indices de la tabla `genero`
---
-ALTER TABLE `genero`
-  ADD PRIMARY KEY (`idgenero`);
-
---
--- Indices de la tabla `gustos`
---
-ALTER TABLE `gustos`
-  ADD PRIMARY KEY (`idgustos`);
-
---
--- Indices de la tabla `registro`
---
-ALTER TABLE `registro`
-  ADD PRIMARY KEY (`codigoestudiante`),
-  ADD UNIQUE KEY `nick` (`nick`),
-  ADD KEY `idcarrera` (`idcarrera`),
-  ADD KEY `idgenero` (`idgenero`);
-
---
--- Indices de la tabla `usuario`
---
-ALTER TABLE `usuario`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `idestudiante` (`idestudiante`),
-  ADD KEY `idgustos` (`idgustos`),
-  ADD KEY `idgenerousuario` (`idgenerousuario`),
-  ADD KEY `idcarrerausuario` (`idcarrerausuario`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `carrera`
---
-ALTER TABLE `carrera`
-  MODIFY `idcarrera` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `chat`
---
-ALTER TABLE `chat`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `genero`
---
-ALTER TABLE `genero`
-  MODIFY `idgenero` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `gustos`
---
-ALTER TABLE `gustos`
-  MODIFY `idgustos` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `usuario`
---
-ALTER TABLE `usuario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- Restricciones para tablas volcadas
---
-
---
--- Filtros para la tabla `chat`
---
-ALTER TABLE `chat`
-  ADD CONSTRAINT `chat_ibfk_1` FOREIGN KEY (`enviado`) REFERENCES `registro` (`codigoestudiante`),
-  ADD CONSTRAINT `chat_ibfk_2` FOREIGN KEY (`recibido`) REFERENCES `registro` (`codigoestudiante`);
-
---
--- Filtros para la tabla `registro`
---
-ALTER TABLE `registro`
-  ADD CONSTRAINT `registro_ibfk_1` FOREIGN KEY (`idcarrera`) REFERENCES `carrera` (`idcarrera`),
-  ADD CONSTRAINT `registro_ibfk_2` FOREIGN KEY (`idgenero`) REFERENCES `genero` (`idgenero`);
-
---
--- Filtros para la tabla `usuario`
-ALTER TABLE `usuario`
-  ADD CONSTRAINT `usuario_ibfk_1` FOREIGN KEY (`idestudiante`) REFERENCES `registro` (`codigoestudiante`),
-  ADD CONSTRAINT `usuario_ibfk_2` FOREIGN KEY (`idgustos`) REFERENCES `gustos` (`idgustos`),
-  ADD CONSTRAINT `usuario_ibfk_3` FOREIGN KEY (`idgenerousuario`) REFERENCES `genero` (`idgenero`),
-  ADD CONSTRAINT `usuario_ibfk_4` FOREIGN KEY (`idcarrerausuario`) REFERENCES `carrera` (`idcarrera`);
-COMMIT;
+CREATE TABLE notificaciones (
+  idnotificacion int(11) PRIMARY KEY,
+  idusuario int(25),
+  tipodenotificacion varchar(50),
+  FOREIGN KEY (idusuario) REFERENCES usuario (id)
+); 
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
